96d29bd7c7f9dabf620ae24ffc98f4064e956218
Adam Ciszewski <adam.cisz@pm.me>
2023-11-18 13:16:39 +0100

    Add project files.

---
+++ SzczecinHackathon/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SzczecinHackathon.Data
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
